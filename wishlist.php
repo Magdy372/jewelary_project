c8d9772b39aa0fd4606103338424c2e4e46a92f6
OmarAbdelmonem1 <omar2108230@miuegypt.edu.eg>
2023-10-18 01:37:47 +0300

    partials

---
+++ wishlist.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Wishlist</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="icon" href="img/favicon.png" />
+        <!-- Place favicon.ico in the root directory -->
+		<!-- google-font -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet"> 
+		<!-- all css here -->
+		<!-- bootstrap v3.3.6 css -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- animate css -->
+        <link rel="stylesheet" href="css/animate.css">
+		<!-- jquery-ui.min css -->
+        <link rel="stylesheet" href="css/jquery-ui.min.css">
+		<!-- nivo-slider css -->
+        <link rel="stylesheet" href="css/nivo-slider.css">
+		<!-- magnific-popup css -->
+        <link rel="stylesheet" href="css/magnific-popup.css">		
+		<!-- meanmenu css -->
+        <link rel="stylesheet" href="css/meanmenu.min.css">
+		<!-- owl.carousel css -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+		<!--linearicons css -->
+        <link rel="stylesheet" href="css/linearicons-icon-font.min.css">
+		<!-- font-awesome css -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+		<!-- style css -->
+		<link rel="stylesheet" href="style.css">
+		<!-- responsive css -->
+        <link rel="stylesheet" href="css/responsive.css" />
+		<!-- modernizr css -->
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        
+		<!-- header-start -->
+		<?php include('partials/header.php'); ?>
+		
+		<!-- mainmenu-area-end -->
+		<!-- page-title-wrapper-start -->
+		<div class="page-title-wrapper">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<div class="page-title">
+							<h3>wishlist</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- page-title-wrapper-end -->
+		<!-- wishlist-area start -->
+		<div class="wishlist-area pt-80 pb-30">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<div class="wishlist-content">
+							<form action="#">
+								<div class="wishlist-title">
+									<h2>My wishlist</h2>
+								</div>
+								<div class="wishlist-table table-responsive">
+									<table>
+										<thead>
+											<tr>
+												<th class="product-remove"><span class="nobr">Remove</span></th>
+												<th class="product-thumbnail">Image</th>
+												<th class="product-name"><span class="nobr">Product Name</span></th>
+												<th class="product-price"><span class="nobr"> Unit Price </span></th>
+												<th class="product-stock-stauts"><span class="nobr"> Stock Status </span></th>
+												<th class="product-add-to-cart"><span class="nobr">add-to-cart </span></th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td class="product-remove"><a href="#">x</a></td>
+												<td class="product-thumbnail"><a href="#"><img src="img/wishlist/1.jpg" alt="" /></a></td>
+												<td class="product-name"><a href="#">Vestibulum suscipit</a></td>
+												<td class="product-price"><span class="amount">$35.00</span></td>
+												<td class="product-stock-status"><span class="wishlist-in-stock">In Stock</span></td>
+												<td class="product-add-to-cart"><a href="#"> Add to Cart</a></td>
+											</tr>
+											<tr>
+												<td class="product-remove"><a href="#">x</a></td>
+												<td class="product-thumbnail"><a href="#"><img src="img/wishlist/2.jpg" alt="" /></a></td>
+												<td class="product-name"><a href="#">Vestibulum dictum magna</a></td>
+												<td class="product-price"><span class="amount">$50.00</span></td>
+												<td class="product-stock-status"><span class="wishlist-in-stock">In Stock</span></td>
+												<td class="product-add-to-cart"><a href="#"> Add to Cart</a></td>
+											</tr>
+										</tbody>
+										<tfoot>
+											<tr>
+												<td colspan="6">
+													<div class="wishlist-share">
+														<h4 class="wishlist-share-title">Share on:</h4>
+														<ul>
+															<li><a class="facebook" href="#"></a></li>
+															<li><a class="twitter" href="#"></a></li>
+															<li><a class="pinterest" href="#"></a></li>
+															<li><a class="googleplus" href="#"></a></li>
+															<li><a class="email" href="#"></a></li>
+														</ul>
+													</div>
+												</td>
+											</tr>
+										</tfoot>
+									</table>
+								</div>	
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- wishlist-area end -->
+		<!-- contact-area-start -->
+		<?php include('partials/footer.php'); ?>
+		<!-- .copyright-area-end -->
+		
+		<!-- all js here -->
+		<!-- jquery latest version -->
+        <script src="js/vendor/jquery-1.12.0.min.js"></script>
+		<!-- bootstrap js -->
+        <script src="js/bootstrap.min.js"></script>
+		<!-- owl.carousel js -->
+        <script src="js/owl.carousel.min.js"></script>
+		<!-- meanmenu js -->
+        <script src="js/jquery.meanmenu.js"></script>
+		<!-- jquery-ui js -->
+        <script src="js/jquery-ui.min.js"></script>
+		<!-- nivo.slider js -->
+        <script src="js/jquery.nivo.slider.js"></script>	
+		<!-- magnific-popup js -->
+        <script src="js/jquery.magnific-popup.min.js"></script>		
+		<!-- wow js -->
+        <script src="js/wow.min.js"></script>
+		<!-- scrolly js -->
+        <script src="js/jquery.scrolly.js"></script>		
+		<!-- plugins js -->
+        <script src="js/plugins.js"></script>
+		<!-- main js -->
+        <script src="js/main.js"></script>
+    </body>
+</html>
