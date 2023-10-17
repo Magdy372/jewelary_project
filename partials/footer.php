2a0d16c0f20caea90accc8ab2504e02badb4fe09
OmarAbdelmonem1 <omar2108230@miuegypt.edu.eg>
2023-10-18 01:18:28 +0300

    partials

---
+++ partials/footer.php
@@ -0,0 +1,196 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Home </title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--<link rel="icon" href="img/favicon.png" />
+         Place favicon.ico in the root directory -->
+		<!-- google-font -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet"> 
+		<!-- all css here -->
+		<!-- bootstrap v3.3.6 css -->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+		
+		<!-- animate css -->
+        <link rel="stylesheet" href="../css/animate.css">
+		<!-- jquery-ui.min css -->
+        <link rel="stylesheet" href="../css/jquery-ui.min.css">
+		<!-- meanmenu css -->
+        <link rel="stylesheet" href="../css/meanmenu.min.css">
+		<!-- nivo-slider css -->
+        <link rel="stylesheet" href="../css/nivo-slider.css">		
+		<!-- owl.carousel css -->
+        <link rel="stylesheet" href="../css/owl.carousel.css">
+		<!--linearicons css -->
+        <link rel="stylesheet" href="../css/linearicons-icon-font.min.css">
+		<!-- font-awesome css -->
+        <link rel="stylesheet" href="../css/font-awesome.min.css">
+		<!-- style css -->
+		<link rel="stylesheet" href="../style.css">
+		<!-- responsive css -->
+        <link rel="stylesheet" href="../css/responsive.css" />
+		
+    </head>
+
+
+
+<!-- feature-preduct-area-end -->
+		<!-- contact-area-start -->
+		<div class="contact-area ptb-40">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 mar_b-30">
+						<div class="contuct-info text-center">
+							<h4>Sign up for news & offers!</h4>
+							<p>You may safely unsubscribe at any time</p>
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-8 col-sm-12 col-lg-offset-1 col-xs-12">
+						<div class="search-box">
+							<form action="#">
+								<input type="email" placeholder="Enter your email address"/>
+								<button><span class="lnr lnr-envelope"></span></button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- contact-area-end -->		
+		<!-- footer-area-start -->
+		<div class="footer-area footer-area-4 ptb-80">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 mar_b-30">
+						<div class="footer-wrapper">
+							<div class="footer-logo">
+								<a href="#"><img src="img/logo-4.png" alt="" /></a>
+							</div>
+							<p>We are a team of designers and developers that create high quality Magento, Prestashop, Opencart themes and provide premium and  support to our products.</p>
+							<ul>
+								<li>
+									<span>Address : </span> Delhi india
+								</li>
+								<li>
+									<span>Phone: </span> 123445689
+								</li>
+								<li>
+									<span>Email:</span> <a href="#">admin@dsfbvbbdvgz.company</a>
+								</li>
+							</ul>
+							<ul class="footer-social">
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-youtube"></i></a></li>
+								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+								<li><a href="#"><i class="fa fa-flickr"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-2 col-md-3 hidden-sm col-xs-12 mar_b-30">
+						<div class="footer-wrapper">
+							<div class="footer-title">
+								<a href="#"><h3>useful links</h3></a>
+							</div>
+							<div class="footer-wrapper">
+								<ul class="usefull-link">
+									<li><a href="#">Contact us</a></li>
+									<li><a href="sitemap.html">Site map</a></li>
+									<li><a href="#">About us</a></li>
+									<li><a href="#">Specials</a></li>
+									<li><a href="#">My Cart</a></li>
+									<li><a href="#">Wishlist</a></li>
+									<li><a href="#">Custom service</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 hidden-md hidden-sm col-xs-12 mar_b-30">
+						<div class="footer-wrapper">
+							<div class="footer-title">
+								<a href="#"><h3>useful links</h3></a>
+							</div>
+							<div class="footer-wrapper">
+								<ul class="usefull-link">
+									<li><a href="#">Contact us</a></li>
+									<li><a href="sitemap.html">Site map</a></li>
+									<li><a href="#">About us</a></li>
+									<li><a href="#">Specials</a></li>
+									<li><a href="#">My Cart</a></li>
+									<li><a href="#">Wishlist</a></li>
+									<li><a href="#">Custom service</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+						<div class="footer-wrapper">
+							<div class="footer-title">
+								<a href="#"><h3>useful links</h3></a>
+							</div>
+							<div class="footer-wrapper">
+								<ul class="usefull-link">
+									<li><a href="#">Contact us</a></li>
+									<li><a href="sitemap.html">Site map</a></li>
+									<li><a href="#">About us</a></li>
+									<li><a href="#">Specials</a></li>
+									<li><a href="#">My Cart</a></li>
+									<li><a href="#">Wishlist</a></li>
+									<li><a href="#">Custom service</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- footer-area-end -->
+		<!-- .copyright-area-start -->
+		<div class="copyright-area copyright-4">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 mar_b-30">
+						<div class="copyright text-left">
+							<p>2016 @ All Rights Reserved - <a href="#"></a></p>
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<div class="copyright-img text-right">
+							<a href="#"><img src="img/payment.png" alt="" /></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+    	<!-- all js here -->
+		<!-- jquery latest version -->
+        <script src="../js/vendor/jquery-1.12.0.min.js"></script>
+		<!-- bootstrap js -->
+        <script src="../js/bootstrap.min.js"></script>
+		<!-- owl.carousel js -->
+        <script src="../js/owl.carousel.min.js"></script>
+		<!-- meanmenu js -->
+        <script src="js/jquery.meanmenu.js"></script>
+		<!-- jquery-ui js -->
+        <script src="../js/jquery-ui.min.js"></script>
+		<!-- wow js -->
+        <script src="../js/wow.min.js"></script>
+		<!-- scrolly js -->
+        <script src="../js/jquery.scrolly.js"></script>		
+		<!-- magnific-popup js -->
+        <script src="../js/jquery.magnific-popup.min.js"></script>	
+		<!--  countdown js -->
+        <script src="../js/jquery.countdown.min.js"></script>	
+		<!-- nivo.slider js -->
+        <script src="../js/jquery.nivo.slider.js"></script>			
+		<!-- plugins js -->
+        <script src="../js/plugins.js"></script>
+		<!-- main js -->
+        <script src="../js/main.js"></script>
