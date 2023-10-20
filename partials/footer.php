e9d04d40a6f2e171ecb1eeb6b8311a3dab260a07
Ahmedhossam26 <125680149+Ahmedhossam26@users.noreply.github.com>
2023-10-20 22:05:36 +0300

    adding some modification in the nav bar

--- partials/footer.php
+++ partials/footer.php
@@ -33,54 +33,44 @@
 		<!-- contact-area-end -->		
 		<!-- footer-area-start -->
 		<div class="footer-area footer-area-4 ptb-80">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 mar_b-30">
-						<div class="footer-wrapper">
-							<div class="footer-logo">
-								<a href="#"><img src="./img/logo-4.png" alt="" /></a>
-							</div>
-							<p>Our Social Media Accounts</p>
-							<ul class="footer-social">
-								<li><a href="https://www.facebook.com/profile.php?id=61551954653039&mibextid=LQQJ4d"><i class="fa fa-facebook"></i></a></li>
-								<li><a href="https://instagram.com/amralhedia?igshid=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr"><i class="fa fa-instagram"></i></a></li>
-							</ul>
-						</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 mar_b-30">
+				<div class="footer-wrapper">
+					<div class="footer-logo">
+						<a href="#"><img src="./img/logo-4.png" alt="" /></a>
 					</div>
-					<div class="col-lg-2 col-md-3 hidden-sm col-xs-12 mar_b-30">
-						<div class="footer-wrapper">
-							<div class="footer-title">
-								<a href="#"><h3>Useful Links</h3></a>
-							</div>
-							<ul class="useful-link">
-								<li><a href="#">Contact Us</a></li>
-								<li><a href="sitemap.php">Site Map</a></li>
-								<li><a href="#">About Us</a></li>
-								<li><a href="#">Specials</a></li>
-								<li><a href="#">My Cart</a></li>
-								<li><a href="#">Wishlist</a></li>
-								<li><a href="#">Custom Service</a></li>
-							</ul>
-						</div>
+					<p>Our Social Media Accounts</p>
+					<ul class="footer-social">
+						<li><a href="https://www.facebook.com/profile.php?id=61551954653039&mibextid=LQQJ4d"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="https://instagram.com/amralhedia?igshid=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr"><i class="fa fa-instagram"></i></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mar_b-30">
+				<div class="footer-wrapper">
+					<div class="footer-title">
+						<a href="#"><h3>Useful Links</h3></a>
+					</div>
+					<ul class="useful-link">
+						<li><a href="#">Contact Us</a></li>
+						<li><a href="sitemap.php">Site Map</a></li>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Specials</a></li>
+						<li><a href="#">My Cart</a></li>
+						<li><a href="#">Wishlist</a></li>
+						<li><a href="#">Custom Service</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-3 col-sm-12 col-xs-12">
+				<div class="footer-wrapper">
+					<p class="rights-reserved">2023 @ All Rights Reserved</p>
+					<div class="visa-logo">
+						<a href="#"><img src="img/payment.png" alt="" /></a>
 					</div>
 				</div>
 			</div>
 		</div>
-		<!-- footer-area-end -->
-		<!-- .copyright-area-start -->
-		<div class="copyright-area copyright-4">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 mar_b-30">
-						<div class="copyright text-left">
-							<p>2023 @ All Rights Reserved - <a href="#"></a></p>
-						</div>
-					</div>
-					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-						<div class="copyright-img text-right">
-							<a href="#"><img src="img/payment.png" alt="" /></a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+	</div>
+</div>
