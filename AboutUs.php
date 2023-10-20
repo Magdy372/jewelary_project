54bb78ed15e32f47f8b052b9c8269b0f94387a23
Ahmedhossam26 <125680149+Ahmedhossam26@users.noreply.github.com>
2023-10-21 00:15:18 +0300

    adding the about us page content and its css

---
+++ AboutUs.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>About Us</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--<link rel="icon" href="img/favicon.png" />
+         Place favicon.ico in the root directory -->
+		<!-- google-font -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet"> 
+		<!-- all css here -->
+		<!-- bootstrap v3.3.6 css -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- animate css -->
+        <link rel="stylesheet" href="css/animate.css">
+		<!-- jquery-ui.min css -->
+        <link rel="stylesheet" href="css/jquery-ui.min.css">
+		<!-- meanmenu css -->
+        <link rel="stylesheet" href="css/meanmenu.min.css">
+		<!-- nivo-slider css -->
+        <link rel="stylesheet" href="css/nivo-slider.css">		
+		<!-- owl.carousel css -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+		<!--linearicons css -->
+        <link rel="stylesheet" href="css/linearicons-icon-font.min.css">
+		<!-- font-awesome css -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+		<!-- style css -->
+		<link rel="stylesheet" href="/css/style.css">
+		<!-- responsive css -->
+        <link rel="stylesheet" href="css/responsive.css" />
+        <style>
+        
+        .about-us-section {
+            padding: 60px 0;
+            background-color: #f9f9f9;
+        }
+
+        .about-us-section h2 {
+            font-family: 'Lato', sans-serif;
+            font-weight: 700;
+            font-size: 36px;
+            color: #333333;
+            margin-bottom: 30px;
+        }
+
+        .about-us-section p {
+            font-family: 'Lato', sans-serif;
+            font-weight: 400;
+            font-size: 16px;
+            color: #666666;
+            line-height: 1.8;
+            margin-bottom: 20px;
+        }
+
+        /* Responsive CSS styles */
+        @media (max-width: 768px) {
+            /* Adjust padding for smaller screens */
+            .about-us-section {
+                padding: 40px 0;
+            }
+        }
+
+        @media (max-width: 576px) {
+            /* Adjust font size for smaller screens */
+            .about-us-section h2 {
+                font-size: 30px;
+            }
+
+            .about-us-section p {
+                font-size: 14px;
+            }
+        }
+    </style>
+		
+</head>
+<body>
+    <?php include('partials/header.php'); ?>
+
+
+    <section class="about-us-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>About Us</h2>
+                <p>Welcome to our jewelry store! We have been proudly serving our customers since 2000. Our store was founded by Mr. Amr El Hedia, a passionate and experienced jeweler with a deep love for exquisite craftsmanship.</p>
+                <p>At our store, we strive to offer a wide selection of high-quality jewelry pieces that cater to different styles and preferences. From elegant necklaces to stunning rings, our collection showcases the finest craftsmanship and attention to detail.</p>
+                <p>We believe that jewelry is not just an accessory but a reflection of one's unique personality and cherished moments. That's why we are committed to providing exceptional customer service and helping you find the perfect piece that tells your story.</p>
+                <p>Whether you're looking for a special gift for a loved one or treating yourself to something beautiful, we're here to guide you through every step of your jewelry journey. Explore our collection and let us assist you in finding the perfect piece that will be treasured for a lifetime.</p>
+                <p>Thank you for choosing our store. We look forward to serving you and being a part of your precious memories.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+    <?php include('partials/footer.php'); ?>
+		<!-- contact-area-end -->		
+		
+		<!-- .copyright-area-end -->
+		
+		<!-- all js here -->
+		<!-- jquery latest version -->
+        <script src="js/vendor/jquery-1.12.0.min.js"></script>
+		<!-- bootstrap js -->
+        <script src="js/bootstrap.min.js"></script>
+		<!-- owl.carousel js -->
+        <script src="js/owl.carousel.min.js"></script>
+		<!-- meanmenu js -->
+        <script src="js/jquery.meanmenu.js"></script>
+		<!-- jquery-ui js -->
+        <script src="js/jquery-ui.min.js"></script>
+		<!-- wow js -->
+        <script src="js/wow.min.js"></script>
+		<!-- scrolly js -->
+        <script src="js/jquery.scrolly.js"></script>		
+		<!-- magnific-popup js -->
+        <script src="js/jquery.magnific-popup.min.js"></script>	
+		<!--  countdown js -->
+        <script src="js/jquery.countdown.min.js"></script>	
+		<!-- nivo.slider js -->
+        <script src="js/jquery.nivo.slider.js"></script>			
+		<!-- plugins js -->
+        <script src="js/plugins.js"></script>
+		<!-- main js -->
+        <script src="js/main.js"></script>
+
+</body>
+</html>
