d848ed1da90ba34555321dd3dbef97d514c30e15
Ahmed21gamal <125803560+Ahmed21gamal@users.noreply.github.com>
2023-10-20 17:47:15 +0300

    update add_admin

--- add_admin.php
+++ add_admin.php
@@ -4,6 +4,36 @@
     <title>Jewelry Website</title>
     <style>
         /* CSS for styling the navbar */
+
+        .container {
+            width: 50%;
+            margin: 0 auto;
+        }
+
+        .form-group {
+            margin-bottom: 20px;
+        }
+
+        label {
+            display: block;
+            font-weight: bold;
+        }
+
+        input[type="text"],
+        input[type="number"],
+        textarea {
+            width: 100%;
+            padding: 10px;
+        }
+
+        button {
+            background-color: #007BFF;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            cursor: pointer;
+        }
+
         .navbar {
             width: 200px;
             height: 100%;
@@ -16,10 +46,10 @@
         }
 
         body {
-            color: #666666;
-            font-family: 'Lato', sans-serif;
-            font-size: 15px;
-            line-height: 1.42857;
+    color: #666666;
+    font-family: 'Lato', sans-serif;
+    font-size: 15px;
+    line-height: 1.42857;
         }
 
         .navbar a {
@@ -34,41 +64,38 @@
             padding: 20px;
         }
     </style>
-
-    <!-- Add Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <div class="navbar">
-        <a href="admin.php">Admin Dashboard</a>
+
+<div class="navbar">
+        <a href="admin_dashboard.php">Admin Dashboard</a>
         <a href="add_admin.php">Add Admin</a>
         <a href="add_product.php">Add Product</a>
         <a href="view_sold_products.php">View Sold Products</a>
     </div>
+    
+<div class="container">
+        <h2>Add a New Admin</h2>
+        <form action="process_product.php" method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="Username">Username</label>
+                <input type="text" name="Username" id="Username" required>
+            </div>
 
-    <!-- Your Bootstrap "Add Admin" Form -->
-    <div class="content">
-        <h2>Add Admin</h2>
-        <form>
+            <form action="process_product.php" method="POST" enctype="multipart/form-data">
             <div class="form-group">
-                <label for="name">Name</label>
-                <input type="text" class="form-control" id="name" name="name" required>
+                <label for="Password">Password</label>
+                <input type="text" name="Password" id="Password" required>
             </div>
+
+            
             <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" name="email" required>
+                <label for="Admin_image">Admin Image</label>
+                <input type="file" name="Admin_image" id="Admin_image" accept="image/*" required>
             </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" id="password" name="password" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Add Admin</button>
+
+            <button type="submit">Add admin</button>
         </form>
     </div>
-
-    <!-- Add Bootstrap and jQuery JavaScript -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
