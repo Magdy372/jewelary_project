f41969a5dd96e9e3e326f8f84e145c72286b7b99
Ahmedhossam26 <125680149+Ahmedhossam26@users.noreply.github.com>
2023-12-19 21:12:10 +0200

    solving the error problem

--- app/model/OrderModel.php
+++ app/model/OrderModel.php
@@ -1,11 +1,12 @@
 <?php
-require_once(_ROOT_ . "model/Model.php");
+require_once(__ROOT__ . "model/Model.php");
+require_once(__ROOT__ . "controller/OrderController.php");
 ?>
 <?php
 
 class OrderModel extends Model
 {
-  
+
 
     public function __construct($userID = null)
     {
@@ -15,109 +16,108 @@ class OrderModel extends Model
         }
     }
     public function createOrder($userID, $totalAmount, $status, $cartDetails, $selectedAddressID)
-{
-    try {
-        // Insert order information into Order_table
-        $sqlOrder = "INSERT INTO Order_table (UserID, AddressID, TotalAmount, Status) 
+    {
+        try {
+            // Insert order information into Order_table
+            $sqlOrder = "INSERT INTO Order_table (UserID, AddressID, TotalAmount, Status) 
                 VALUES ('$userID', '$selectedAddressID', '$totalAmount', '$status')";
-        $this->db->query($sqlOrder);
+            $this->db->query($sqlOrder);
 
-        // Get the last inserted order ID
-        $orderID = $this->getLastInsertId();
-        echo "orderID";
-        
+            // Get the last inserted order ID
+            $orderID = $this->getLastInsertId();
+            echo "orderID";
 
-        // Insert order details into OrderDetails for each product in the cart
-        foreach ($cartDetails as $product) {
-            $productID = $product['ProductID'];
-            $quantity = $product['Quantity'];
-            $subtotal = $product['Subtotal'];
 
-            $sqlOrderDetails = "INSERT INTO OrderDetails (OrderID, ProductID, Quantity, Subtotal) 
+            // Insert order details into OrderDetails for each product in the cart
+            foreach ($cartDetails as $product) {
+                $productID = $product['ProductID'];
+                $quantity = $product['Quantity'];
+                $subtotal = $product['Subtotal'];
+
+                $sqlOrderDetails = "INSERT INTO OrderDetails (OrderID, ProductID, Quantity, Subtotal) 
                                VALUES ('$orderID', '$productID', '$quantity', '$subtotal')";
-            $this->db->query($sqlOrderDetails);
-        }
+                $this->db->query($sqlOrderDetails);
+            }
 
 
 
-        return $orderID; // Return the ID of the created order
-    } catch (Exception $e) {
-        // Log the exception message
-        error_log("Error creating order: " . $e->getMessage());
-        return false; // Return false to indicate an error
-    }
-}
-
-    
-private function getLastInsertId()
-{
-    $result = $this->db->query("SELECT LAST_INSERT_ID() AS last_insert_id");
-    
-    if ($result && $row = $result->fetch_assoc()) {
-        return $row['last_insert_id'];
-    }
-
-    return null; // Return null or handle the case where the result is empty
-}
-
-public function getOrdersByUserID($userID)
-{
-    // Retrieve orders by UserID from Order_table
-    $sql = "SELECT * FROM Order_table WHERE UserID = '$userID'";
-    $result = $this->db->query($sql);
-
-    $orders = array();
-
-    if ($result) {
-        while ($row = $result->fetch_assoc()) {
-            $orderID = $row['OrderId'];
-           
-
-           
-            $order = array(
-                'OrderID' => $orderID,
-                'AddressID' => $row['AddressID'],
-                'TotalAmount' => $row['TotalAmount'],
-                'Status' => $row['Status'],
-                
-            );
-
-            $orders[] = $order;
+            return $orderID; // Return the ID of the created order
+        } catch (Exception $e) {
+            // Log the exception message
+            error_log("Error creating order: " . $e->getMessage());
+            return false; // Return false to indicate an error
         }
     }
 
-    return $orders;
-}
 
-public function getOrders()
-{
-    // Retrieve orders by UserID from Order_table
-    $sql = "SELECT * FROM Order_table";
-    $result = $this->db->query($sql);
+    private function getLastInsertId()
+    {
+        $result = $this->db->query("SELECT LAST_INSERT_ID() AS last_insert_id");
 
-    $orders = array();
-
-    if ($result) {
-        while ($row = $result->fetch_assoc()) {
-            $orderID = $row['OrderId'];
-           
-
-           
-            $order = array(
-                'OrderID' => $orderID,
-                'AddressID' => $row['AddressID'],
-                'TotalAmount' => $row['TotalAmount'],
-                'Status' => $row['Status'],
-                
-            );
-
-            $orders[] = $order;
+        if ($result && $row = $result->fetch_assoc()) {
+            return $row['last_insert_id'];
         }
+
+        return null; // Return null or handle the case where the result is empty
     }
 
-    return $orders;
-}
+    public function getOrdersByUserID($userID)
+    {
+        // Retrieve orders by UserID from Order_table
+        $sql = "SELECT * FROM Order_table WHERE UserID = '$userID'";
+        $result = $this->db->query($sql);
 
+        $orders = array();
+
+        if ($result) {
+            while ($row = $result->fetch_assoc()) {
+                $orderID = $row['OrderId'];
+
+
+
+                $order = array(
+                    'OrderID' => $orderID,
+                    'AddressID' => $row['AddressID'],
+                    'TotalAmount' => $row['TotalAmount'],
+                    'Status' => $row['Status'],
+
+                );
+
+                $orders[] = $order;
+            }
+        }
+
+        return $orders;
+    }
+
+    public function getOrders()
+    {
+        // Retrieve orders by UserID from Order_table
+        $sql = "SELECT * FROM Order_table";
+        $result = $this->db->query($sql);
+
+        $orders = array();
+
+        if ($result) {
+            while ($row = $result->fetch_assoc()) {
+                $orderID = $row['OrderId'];
+
+
+
+                $order = array(
+                    'OrderID' => $orderID,
+                    'AddressID' => $row['AddressID'],
+                    'TotalAmount' => $row['TotalAmount'],
+                    'Status' => $row['Status'],
+
+                );
+
+                $orders[] = $order;
+            }
+        }
+
+        return $orders;
+    }
 }
 class OrderDetails extends Model
 {
@@ -169,3 +169,4 @@ class OrderDetails extends Model
         return $details;
     }
 }
+?>
